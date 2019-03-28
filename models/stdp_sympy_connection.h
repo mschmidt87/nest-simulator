6e1f3ce4b828481b8c63884cd196e4025315a214
Maximilian <maximilian@ascent.ai>
2019-03-28 09:16:34 +0900

    stdp_sympy_connection: In update functions, call function argument instead of class member

--- models/stdp_sympy_connection.h
+++ models/stdp_sympy_connection.h
@@ -219,7 +219,7 @@ private:
     // double norm_w = ( w / Wmax_ )
     //   + ( lambda_ * std::pow( 1.0 - ( w / Wmax_ ), mu_plus_ ) * kplus );
     // weight_ += lambda_ * Wmax_ * expr_facilitate_.eval( { weight_ / Wmax_, Kplus_ * std::exp( minus_dt / tau_plus_ ) } );
-    double delta_w = expr_facilitate_.eval( { weight_ / Wmax_, kplus } );
+    double delta_w = expr_facilitate_.eval( { w / Wmax_, kplus } );
     double norm_w = ( w / Wmax_ ) + lambda_ * delta_w;
     return norm_w < 1.0 ? norm_w * Wmax_ : Wmax_;
   }
@@ -229,7 +229,7 @@ private:
   {
     // double norm_w = ( w / Wmax_ )
     //   - ( alpha_ * lambda_ * std::pow( w / Wmax_, mu_minus_ ) * kminus );
-    double delta_w = expr_depress_.eval( { weight_ / Wmax_, kminus } );
+    double delta_w = expr_depress_.eval( { w / Wmax_, kminus } );
     double norm_w = ( w / Wmax_ ) - alpha_ * lambda_ * delta_w;
     return norm_w > 0.0 ? norm_w * Wmax_ : 0.0;
   }
